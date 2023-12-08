6d3e9d983d2279d9ca7d31d1836825d6dd7c6524
SamS <coding_sams@tuta.io>
2023-12-08 13:09:50 +0100

    day 05 part 2: naive approach

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -140,8 +140,104 @@ where
     }
 }
 
+fn puzzle02<P>(filename: P) -> Result<u64, String>
+where
+    P: AsRef<Path>,
+{
+    let file = match File::open(filename) {
+        Ok(file) => file,
+        Err(_) => return Err(String::from("Error opening file")),
+    };
+    let mut buf = BufReader::new(file).lines();
+
+    // read in seeds:
+    let seed_regex = Regex::new(r"(\d+) (\d+)").unwrap();
+    let seed_vec: Vec<(u64, u64)> = match buf.next() {
+        Some(result) => match result {
+            Ok(line) => seed_regex
+                .captures_iter(&line)
+                .filter_map(|c| {
+                    let c1 = c[1].parse::<u64>();
+                    let c2 = c[2].parse::<u64>();
+                    if c1.is_ok() && c2.is_ok() {
+                        Some((c1.unwrap(), c2.unwrap()))
+                    } else {
+                        None
+                    }
+                })
+                .collect(),
+            Err(_) => return Err(String::from("Could not parse seed line")),
+        },
+        None => return Err(String::from("Seed line does not exist")),
+    };
+
+    buf.next();
+
+    let mapping_routine_vec = vec![
+        (
+            parse_mapping_section("seed-to-soil map:", &mut buf)?,
+            "seed-to-soil map",
+        ),
+        (
+            parse_mapping_section("soil-to-fertilizer map:", &mut buf)?,
+            "soil-to-fertilizer",
+        ),
+        (
+            parse_mapping_section("fertilizer-to-water map:", &mut buf)?,
+            "fertilizer-to-water",
+        ),
+        (
+            parse_mapping_section("water-to-light map:", &mut buf)?,
+            "water-to-light",
+        ),
+        (
+            parse_mapping_section("light-to-temperature map:", &mut buf)?,
+            "light-to-temperature",
+        ),
+        (
+            parse_mapping_section("temperature-to-humidity map:", &mut buf)?,
+            "temperature-to-humidity",
+        ),
+        (
+            parse_mapping_section("humidity-to-location map:", &mut buf)?,
+            "humidity-to-location",
+        ),
+    ];
+
+    let mut lowest_location_number: Option<u64> = None;
+    for (seed_start, seed_range) in seed_vec {
+        println!("Starting seed: {} Range: {}", seed_start, seed_range);
+        for seed_num in seed_start..seed_start + seed_range {
+            let mut location_number = seed_num;
+            for (map_vec, map_vec_name) in &mapping_routine_vec {
+                let mut v: Vec<u64> = map_vec.iter().filter_map(|f| f(location_number)).collect();
+                if v.len() == 1 {
+                    location_number = v.pop().unwrap();
+                } else if 1 < v.len() {
+                    return Err(format!(
+                    "There was more than one function mapping for the seed. Number of mappings: {}. Mapping Name: {}", v.len(), map_vec_name
+                ));
+                }
+            }
+            match lowest_location_number {
+                Some(number) => {
+                    if location_number < number {
+                        lowest_location_number = Some(location_number);
+                    }
+                }
+                None => lowest_location_number = Some(location_number),
+            }
+        }
+    }
+    match lowest_location_number {
+        Some(number) => Ok(number),
+        None => Err(String::from("no lowest location number found")),
+    }
+}
+
 fn main() {
     println!("Solution: {}", puzzle01("puzzle01_input").unwrap());
+    println!("Solution: {}", puzzle02("puzzle01_input").unwrap());
 }
 
 #[cfg(test)]
@@ -149,5 +245,9 @@ mod tests {
     #[test]
     fn test1() {
         assert_eq!(crate::puzzle01("puzzle01_input_test").unwrap(), 35)
+    }
+    #[test]
+    fn test2() {
+        assert_eq!(crate::puzzle02("puzzle01_input_test").unwrap(), 46)
     }
 }
